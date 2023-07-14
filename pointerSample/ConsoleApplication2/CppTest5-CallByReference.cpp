efee5ee658b2d5355c04cfb2e4954737e88db397
Hoya-Choi <csh0278@gmail.com>
2023-07-14 16:06:22 +0900

    C++ Test

---
+++ pointerSample/ConsoleApplication2/CppTest5-CallByReference.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+void swap(int &a, int &b) //call-by-reference
+{
+	int temp = a; //temp는 임시로 저장할 변수
+	printf("a: %d, b: %d\n", a, b);
+	a = b;
+	printf("a: %d, b: %d\n", a, b);
+	b = temp;
+	printf("a: %d, b: %d\n", a, b);
+}
+int main()
+{
+	int i = -100;
+	int j = 100;
+	swap(i, j);
+}
--- pointerSample/CppTest3-Macro/CppTest3-Macro.cpp
+++ pointerSample/CppTest3-Macro/CppTest3-Macro.cpp
@@ -1,10 +1,10 @@
 ﻿#include <stdio.h>
-#define	abs(val) (val < 0) ? -val : val //매크로 함수 실수,정수 다 가능
+#define	abs(val) ((val) < 0) ? -(val) : (val) //매크로 함수 실수,정수 다 가능
 
 int main()
 {
 	int i = -1;
 	float a = -2.0;
-	printf("i = %d -> i의 절대값은 %d\n", i, abs(i));
-	printf("a = %f -> a의 절대값은 %f\n", a, abs(a));//오버로딩 확인
+	printf("i = %d -> i의 절대값은 %d\n", i, abs(-7)); //디폴트 값 넣을시 매크로 함수 변수에 괄호 달기
+	printf("a = %f -> a의 절대값은 %f\n", a, abs(a));//매크로 확인
 }
